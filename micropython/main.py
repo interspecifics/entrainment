dd3b0385eac707bcab472fe767e37e13dea618b9
Emme Ang <mecanosauros@gmail.com>
2024-05-24 09:34:42 -0600

    update

--- micropython/main.py
+++ micropython/main.py
@@ -4,12 +4,14 @@ from uosc.client import Bundle, Client, create_message
 from neopixel import NeoPixel
 
 # config params ---------------------------------------------
-id = 2
+id = 4
 advolt = 6500
+#osc_ip = '192.168.0.103'
+#osc_port = 8000
 osc_ip = '192.168.1.84'
 osc_port = 8000
-#osc_ip2 = '192.168.0.105'
-#osc_port2 = 8000
+osc_ip2 = '192.168.1.96'
+osc_port2 = 8001
 
 #
 #ssid = "INFINITUM2064"
@@ -83,8 +85,8 @@ def do_connect(ssid, password, tries=150):
 do_connect(ssid, passw)
 osc = Client(osc_ip, osc_port)
 osc.send(msg_route + '/start', 1)
-#osc2 = Client(osc_ip2, osc_port2)
-#osc2.send(msg_route + '/start', 1)
+osc2 = Client(osc_ip2, osc_port2)
+osc2.send(msg_route + '/start', 1)
 
 
 
@@ -99,42 +101,80 @@ npx.write()
 sleep(0.2)
 npx[0] = co1
 npx.write()
-isup = 0
 
-past = 0
-t0 = time()
-pot_value = 0
-#
-#while True:
-#  osc.send(msg_route, 1)      
-#  sleep(0.25)
-  
-#"""
+
+def moving_average_detection(ecg_signal, past_signals, past_mean_sum, n=30, threshold=-0.2):
+    """
+    Analyze ECG signal to detect heart beat by looking at the pondered signal diferential.
+
+    This function calculates a processed value by comparing the current ECG signal data 
+    point to the moving average of the last `n` data points in the past_signals list. 
+    The moving average is adjusted dynamically with each new data point. 
+    A processed value is deemed significant and indicative of a potential heart event 
+    if it is below a predefined `threshold`.
+    
+    The algorithm is complexity O(1)  :)
+
+    Args:
+    ecg_signal (float): The current ECG signal data point.
+    past_signals (list of float): A historical list of ECG signal data points.
+    past_mean_sum (float): The cumulative sum of the last `n` ECG signals, used to compute the moving average efficiently.
+    n (int): The number of data points to consider in the moving average.
+    threshold (float): The critical value below which a signal is considered significantly deviant.
+
+    Returns:
+    float: The normalized deviation of the current signal from the moving average.
+    bool: A flag indicating whether the deviation is significant (True if below the threshold).
+    """
+    
+    if len(past_signals) < n:
+        past_mean_sum += ecg_signal
+        past_signals.append(ecg_signal)
+        return 0, False, past_signals, past_mean_sum  # Not enough data to process
+
+    # Update the list of past signals
+    past_signals.append(ecg_signal)
+
+    # Update the moving average sum in O(1) time complexity
+    mean_past_n = float(past_mean_sum / n)
+    past_mean_sum = past_mean_sum - past_signals[0] + past_signals[-1]
+
+    if len(past_signals) > n:
+        past_signals.pop(0)
+
+    # Calculate the difference from the last signal
+    signal_diff = ecg_signal - past_signals[-2]
+    processed_value = float(signal_diff / mean_past_n) if mean_past_n != 0 else 0
+
+    # Check if the processed value is below the threshold
+    is_heart_beat = processed_value < threshold
+
+    return processed_value, is_heart_beat, past_signals, past_mean_sum
+
+
+# variables for processing algorithm
+past_signals = []
+past_mean_sum = 0
+ignore_window_len = 15
+
+ignore_counter = 0
+
 while True:
   try:
-    past = pot_value
     pot_value = pot.read()
-    derivada = pot_value - past
-    print(pot_value)
-    #osc.send(msg_route_g, pot_value)      
-    #osc2.send(msg_route_g, pot_value)
-    #t1 = time() - t0
-    if (pot_value > 5900 and isup == 0):
-      osc.send(msg_route, 1, pot_value)
-      #osc2.send(msg_route, 1)
-      #t0 = time()
-      isup = 1
-      sleep(0.025)
-    else:
-      osc.send(msg_route, 0, pot_value)
-    if (pot_value < 5300 and isup == 1):
-      #osc.send(msg_route, 0)
-      #osc2.send(msg_route, 0)
-      isup = 0
 
-    #else:
-    #    osc.send(msg_route, 0)
-    #sleep(0.01)
-  except:
-      print('somethon gone bad [x.x]')
+    processed_pot_val, is_heart_beat, past_signals, past_mean_sum  = moving_average_detection(pot_value, past_signals, past_mean_sum, n=30, threshold=-0.15)
+
+    if (is_heart_beat and ignore_counter == 0):
+      osc.send(msg_route, 1, pot_value, processed_pot_val)
+      osc2.send(msg_route, 1)
+      ignore_counter = ignore_window_len # reinitialize counter
+    else:
+      osc.send(msg_route, 0, pot_value, processed_pot_val)
+      if ignore_counter > 0:
+         ignore_counter = ignore_counter - 1 # reduce counter
+
+    sleep(0.002)
+  except Exception as e:
+      print('somethon gone bad [x.x]',e)
 #"""
